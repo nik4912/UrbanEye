2a79716ee480bca60158710027334be6b4c50399
Harsh Jadhav <harshjadhavconnect@gmail.com>
2025-04-01 19:30:23 +0000

    Refactor constants in utils to remove unused API endpoints

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -5,7 +5,3 @@ export const SEND_SIGNUP_DATA = `${AUTH_API}/send-signup-data`;
 
 export const DATA_API = "api/data";
 export const FETCH_USER_INFO = `${DATA_API}/fetch-user-info`;
-export const CREATE_ANNOUCEMENT = `${DATA_API}/create-announcement`;
-export const FETCH_ANNOUNCEMENTS = `${DATA_API}/fetch-announcements`;
-export const CREATE_TASKS = `${DATA_API}/create-task`;
-export const FETCH_TASKS = `${DATA_API}/fetch-task`;
